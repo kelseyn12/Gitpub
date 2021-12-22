0e19b98d84099c7dc17a07aed2fb249d3cf7b356
kelsey nocek <kelseyn12@hotmail.com>
2021-12-21 19:00:49 -0600

    set up database

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const drinks = require('./models/drinks.js');
 const app = express();
 const port = 3000;
 
 
-app.get('/gitpub/', (req, res) => {
+
+
+
+app.get('/drinks/', (req, res) => {
     res.send('Welcome to the Gitpub App!')
+});
+app.get('/drinks/:index', (req, res) => {
+    res.send(drinks[req.params.index]);
 });
 
 
